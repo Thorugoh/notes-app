607314aa514cef11c4e10f80c90f2cb2bfee7329
Victor Hugo <pavictorhugo@gmail.com>
2023-09-01 07:40:15 -0300

    feat: edit notes

--- app/src/main/java/com/thorugoh/notes/MainActivity.kt
+++ app/src/main/java/com/thorugoh/notes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.thorugoh.notes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -11,19 +12,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thorugoh.notes.components.SubNote
 import com.thorugoh.notes.ui.theme.NotesTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,34 +45,80 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Note(
+    val id: String,
+    val title: String,
+    val content: String,
+    val done: Boolean
+)
+
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Main(){
+fun Main() {
     NotesTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            data class Note (
-                val title: String,
-                val content: String,
-                val done: Boolean
-            )
+            val focusManager = LocalFocusManager.current
+
+            var notes = remember { mutableStateListOf<Note>().apply {
+                add(Note("teste-1", "title", "content 1", true))
+                add(Note("teste-2", "title 2", "content 2", false))
+            } }
+
+            fun updateNoteTitle(index: Int, text: String) {
+                val note = notes[index]
+                notes[index] = note.copy(title = text)
+            }
+
+            fun updateNoteContent(index: Int, text: String) {
+                val note = notes[index]
+                notes[index] = note.copy(content = text)
+            }
+
+            fun selectNote(index: Int, note: Note){
+                notes[index] = note.copy(done = !note.done)
+            }
+
             LazyColumn {
                 stickyHeader {
                     Row(
-                        Modifier.fillMaxWidth().background(Color.White).padding(end = 16.dp, start = 16.dp, top = 16.dp),
+                        Modifier
+                            .fillMaxWidth()
+                            .background(Color.White)
+                            .padding(end = 16.dp, start = 16.dp, top = 16.dp),
                         horizontalArrangement = Arrangement.End
                     ) {
-                        AddSubNote()
+                        AddSubNote {
+                            notes.add(
+                                Note(
+                                    "teste-${notes.size + 1}",
+                                    "",
+                                    "",
+                                    false
+                                )
+                            )
+                        }
                     }
-
                 }
-                items(
-                    count = 1
-                ) {
-                    SubNote()
+                itemsIndexed(
+                    items = notes,
+                ) {index, it ->
+                    val (id, title, content, selected) = it
+                    SubNote(title, content, selected,
+                        onSelect = {
+                            focusManager.clearFocus()
+                            selectNote(index, it)
+                        },
+                        onTitleChange = {text ->
+                            updateNoteTitle(index, text)
+                        },
+                        onContentChange = { text ->
+                            updateNoteContent(index, text)
+                        }
+                    )
                 }
             }
         }
@@ -69,14 +126,14 @@ fun Main(){
 }
 
 @Composable
-fun AddSubNote(){
-    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 16.dp)) {
+fun AddSubNote(onClick: () -> Unit = {}) {
+    Button(onClick, modifier = Modifier.padding(start = 16.dp)) {
         Text(text = "Add SubNote")
     }
 }
 
 @Preview
 @Composable
-fun Preview(){
+fun Preview() {
     Main()
 }
--- app/src/main/java/com/thorugoh/notes/components/SubNote.kt
+++ app/src/main/java/com/thorugoh/notes/components/SubNote.kt
@@ -1,12 +1,16 @@
 package com.thorugoh.notes.components
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -15,6 +19,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,25 +27,28 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SubNote(){
-    val selected = remember {
-        mutableStateOf(false)
-    }
-    val title = remember {
-        mutableStateOf("")
-    }
-    val content = remember {
-        mutableStateOf("")
-    }
+fun SubNote(
+    title: String,
+    content: String,
+    selected: Boolean,
+    onSelect: ()-> Unit = {},
+    onTitleChange: (text: String) -> Unit = {},
+    onContentChange: (text: String) -> Unit = {}
+) {
 
     Column(
         modifier = Modifier
@@ -53,25 +61,25 @@ fun SubNote(){
             .padding(12.dp),
 
 
-        ){
+        ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            RadioButton(selected.value, {
-                selected.value = !selected.value
-            },
+            RadioButton(selected, onClick = onSelect,
                 Modifier
                     .height(16.dp)
                     .padding(0.dp)
             )
             BasicTextField(
-                value = title.value,
-                onValueChange = {
-                    title.value = it
-                },
-                textStyle = TextStyle( fontSize = 16.sp, fontWeight = FontWeight.Bold, textDecoration = if(selected.value) TextDecoration.LineThrough else TextDecoration.None),
+                value = title,
+                onValueChange = onTitleChange,
+                textStyle = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = if (selected) TextDecoration.LineThrough else TextDecoration.None
+                ),
 
-            )
+                )
         }
         Divider(
             modifier =
@@ -83,14 +91,12 @@ fun SubNote(){
         )
         Row() {
             BasicTextField(
-                value = content.value,
-                onValueChange = {
-                    content.value = it
-                },
+                value = content,
+                onValueChange = onContentChange,
                 modifier = Modifier.fillMaxSize(),
                 textStyle = TextStyle(
                     fontSize = 14.sp,
-                    textDecoration = if(selected.value) TextDecoration.LineThrough else TextDecoration.None
+                    textDecoration = if (selected) TextDecoration.LineThrough else TextDecoration.None
                 ),
             )
         }
@@ -99,6 +105,6 @@ fun SubNote(){
 
 @Preview
 @Composable
-fun SubNotePreview () {
-    SubNote()
+fun SubNotePreview() {
+    SubNote("teste", "content", true)
 }
